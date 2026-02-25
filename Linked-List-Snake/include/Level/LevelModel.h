6aa051e48646dc46688b2fc46595c85fa14cf65f
Viresh_Jadhav <168194192+vireshjadhav@users.noreply.github.com>
2026-02-25 15:07:51 +0530

    feat: configure level two obstacle layout
    1. Set up level_two_element_list in LevelModel based on the provided obstacle layout image to configure level two.

--- Linked-List-Snake/include/Level/LevelModel.h
+++ Linked-List-Snake/include/Level/LevelModel.h
@@ -26,8 +26,54 @@ namespace Level
 		void render();
 
 
-		std::vector<Element::ElementData> level_one_element_list;
-		std::vector<Element::ElementData> level_two_element_list;
+		std::vector<Element::ElementData> level_one_element_list = {};
+		std::vector<Element::ElementData> level_two_element_list = 
+		{
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(1,1)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(2,1)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(3,1)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(1,2)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(1,3)),
+
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(48,1)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(48,2)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(48,3)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(47,1)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(46,1)),
+
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(1,26)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(1,25)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(1,24)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(2,26)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(3,26)),
+
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(48,26)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(48,25)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(48,24)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(47,26)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(46,26)),
+
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(21,11)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(22,11)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(23,11)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(24,11)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(25,11)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(26,11)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(27,11)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(28,11)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(29,11)),
+
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(21,15)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(22,15)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(23,15)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(24,15)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(25,15)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(26,15)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(27,15)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(28,15)),
+			Element::ElementData(Element::ElementType::OBSTACLE, sf::Vector2i(29,15)),
+		};
+
 		std::vector<LevelData> level_configurations;
 
 		float getCellWidth();
